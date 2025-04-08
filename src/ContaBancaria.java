5519d3cb7892f9b4c329ae7d683a0ca18da06100
RevRicardo <revricardolacerda@gmail.com>
2025-04-08 10:07:51 -0300

    Implementa método transferir e tratamento de erro

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ContaBancaria {
     public static void main(String[] args) {
-        // Configurando o formato de moeda para o Brasil
+
         Locale brasil = new Locale("pt", "BR");
         NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(brasil);
 
@@ -13,7 +13,6 @@ public class ContaBancaria {
         String nomeDoCliente = "Ricardo Lacerda";
         String tipoConta = "Conta Corrente";
         int operacao;
-        double valor;
         double saldoDaConta = 2000.00;
 
         do {
@@ -23,25 +22,15 @@ public class ContaBancaria {
 
             switch (operacao) {
                 case 1 -> { System.out.println("O saldo da conta é " + formatoMoeda.format(saldoDaConta)); }
-                case 2 -> {
-                    System.out.print("Qual valor a receber: ");
-                    valor = leitura.nextDouble();
-                    saldoDaConta += valor;
-                    System.out.println("O saldo da conta é " + formatoMoeda.format(saldoDaConta));
-                }
-                case 3 -> {
-                    System.out.println("Operação de transferência ainda não implementada.");
-                }
-                case 4 -> {
-                    System.out.println("Encerrando o programa...");
-                }
-                default -> {
-                    System.out.println("Operação inválida. Tente novamente.");
-                }
+                case 2 -> { saldoDaConta = receberValor(leitura, saldoDaConta, formatoMoeda); }
+                case 3 -> { saldoDaConta = sacarValor(leitura, saldoDaConta, formatoMoeda); }
+                case 4 -> { break; }
+                default -> { System.out.println("Operação inválida. Tente novamente."); }
             }
             System.out.println();
         } while (operacao != 4);
     }
+
     public static void cabecalhoMenu(String nomeDoCliente, String tipoConta) {
         String cabecalho = String.format("""
                 **************************************
@@ -55,5 +44,49 @@ public class ContaBancaria {
                 4 - Sair
                 """, nomeDoCliente, tipoConta);
         System.out.println(cabecalho);
+    }
+
+    // MÉTODO RECEBER
+    public static double receberValor(Scanner leitura, double saldoDaConta, NumberFormat formatoMoeda) {
+        while (true) {
+            try {
+                System.out.print("Qual valor a receber: ");
+                double valor = leitura.nextDouble();
+                System.out.println("O saldo da conta é " + formatoMoeda.format((saldoDaConta += valor)));
+                return saldoDaConta;
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Ao digitar o valor use  ,  para separar as casas decimais..");
+                leitura.nextLine();
+            }
+        }
+    }
+
+    // MÉTODO SACAR
+    public static double sacarValor(Scanner leitura, double saldoDaConta, NumberFormat formatoMoeda) {
+        while (true) {
+            try {
+                System.out.print("Qual valor deseja sacar: ");
+                double valor = leitura.nextDouble();
+                if (valor > saldoDaConta) {
+                    System.out.println("A conta não tem saldo suficiente para essa operação.");
+                    System.out.println("O saldo da conta é " + formatoMoeda.format(saldoDaConta));
+                    System.out.print("Deseja tentar novamente? (S = sim / N = não): ");
+                    leitura.nextLine();
+                    String resposta = leitura.nextLine();
+                    if (resposta.equalsIgnoreCase("n")) {
+                        System.out.println("Encerrando saque...");
+                        break;
+                    }
+                } else {
+                    System.out.println("Saque realizado com sucesso!");
+                    System.out.println("O saldo da conta é " + formatoMoeda.format((saldoDaConta -= valor)));
+                    break;
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Ao digitar o valor use  ,  para separar as casas decimais.");
+                leitura.nextLine();
+            }
+        }
+        return saldoDaConta;
     }
 }
